3172a6edb2cd60bea2fc9801cf525da3c8694772
kaungphyowai <kaungphyowai.mm@gmail.com>
2021-11-23 15:52:59 +0630

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import Player from "./Player";
 import Admin from "./Admin";
-
+import firebaseApp from './firebase/firebaseApp';
+import { getDatabase } from "firebase/database";
 //https://www.youtube.com/watch?v=jNQXAC9IVRw&ab_channel=jawed
 //https://www.youtube.com/watch?v=XbqFZMIidZI&ab_channel=PopCornRest-TikTok
 //https://www.youtube.com/watch?v=hS5CfP8n_js&ab_channel=Mr.Monk
+
+let database = getDatabase(firebaseApp)
 function App() {
   return (
     <div>
       <h1>Hello world</h1>
-      <Player />
+      <Player db={database} />
       <hr />
-      <Admin />
+      <Admin db={database} />
     </div>
   );
 }
---
+++ src/firebase/firebaseApp.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXrdBVeyDwU81QVfllPycfiGtiAjAnrpE",
+  authDomain: "react--queue-player.firebaseapp.com",
+  projectId: "react--queue-player",
+  storageBucket: "react--queue-player.appspot.com",
+  messagingSenderId: "393257056440",
+  appId: "1:393257056440:web:7686440fec372fb4961c4d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
