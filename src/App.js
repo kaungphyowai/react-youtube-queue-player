31d5314c4f26cc931177087ad6fefc207c178462
Kaung-Phyo-Wai <kaungphyowaidecember@gmail.com>
2021-11-26 22:54:25 +0630

    react router added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import firebaseApp from './firebase/firebaseApp';
 import { getDatabase } from "firebase/database";
 import {ref, onValue} from 'firebase/database';
 import { useState, useEffect } from "react";
+import { Link, Routes, Route } from "react-router-dom";
 //https://www.youtube.com/watch?v=jNQXAC9IVRw&ab_channel=jawed
 //https://www.youtube.com/watch?v=XbqFZMIidZI&ab_channel=PopCornRest-TikTok
 //https://www.youtube.com/watch?v=hS5CfP8n_js&ab_channel=Mr.Monk
@@ -31,10 +32,20 @@ function App() {
 
   return (
     <div>
-      <h1>Hello world</h1>
-      <Player database={database} videos={videos}  />
-      <hr />
-      <Admin database={database} videos={videos} />
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/player">Player</Link> |{" "}
+        <Link to="/submit">Video ထည့်မယ်ဗျို့</Link>
+      </nav>
+      <Routes>
+        <Route path='player' element={<Player database={database} videos={videos}  />} />
+        <Route path='submit' element={<Admin database={database} videos={videos} />} />
+      </Routes>
+        
     </div>
   );
 }
